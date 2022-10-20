6c6a39bac776a1c4a6ca3e32842589b6ac07622f
Dimitar <wopz1337@gmail.com>
2022-10-20 12:06:59 +0300

    Fetch All Posts And Save In State

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Posts = () => {
-  return <div className="posts">Posts</div>;
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => setPosts(json));
+  }, []);
+  console.log(posts);
+  return (
+    <div className="posts">
+      {posts.map((post) => {
+        return (
+          <>
+            <h1>{post.title}</h1>
+            <p>{post.body} </p>
+          </>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Posts;
