d1874487d2c7525b888df0cd44f7f41658be266a
Dimitar <wopz1337@gmail.com>
2022-10-20 12:18:50 +0300

    Fetch All Users And Display User Full Name

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Users = () => {
-  return <div className="users">Users</div>;
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((json) => setUsers(json));
+  }, []);
+  console.log(users);
+  return (
+    <>
+      <div className="users">
+        <h2>Users:</h2>
+        {users.map((user) => {
+          return (
+            <>
+              <div>{user.name}</div>
+            </>
+          );
+        })}
+      </div>
+    </>
+  );
 };
 
 export default Users;
