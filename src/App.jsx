b93e2d48372e800b62b4aac92612e2f0b525b7f6
Dimitar <wopz1337@gmail.com>
2022-10-22 11:48:33 +0300

    Added NavBar Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import NavBar from "./components/NavBar";
 import Posts from "./components/Posts";
 import Users from "./components/Users";
 
 function App() {
   return (
     <div className="container">
+      <NavBar />
       <Posts />
       <Users />
     </div>
