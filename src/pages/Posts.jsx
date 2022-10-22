dbbfc6d1d3c91a967ce5b7beb0f255ca3ae17cba
Dimitar <wopz1337@gmail.com>
2022-10-23 01:52:34 +0300

    Added Search Filter By Post Title

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -4,12 +4,14 @@ import {
   CardActionArea,
   CardContent,
   Card,
+  TextField,
 } from "@mui/material";
 import { useEffect, useState } from "react";
 import AddPost from "../components/AddPost";
 
 const Posts = () => {
   const [posts, setPosts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
   useEffect(() => {
     fetch(
       "https://my-json-server.typicode.com/dimitarsabev00/Frond-End-FE-Tech-Challenge/posts"
@@ -21,6 +23,15 @@ const Posts = () => {
   return (
     <>
       <AddPost setPosts={setPosts} />
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <TextField
+          sx={{ width: "40rem" }}
+          placeholder="Search.. "
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+        />
+      </Box>
       <Box
         sx={{
           display: "flex",
@@ -31,22 +42,32 @@ const Posts = () => {
           paddingBottom: "4rem",
         }}
       >
-        {posts.map((post) => {
-          return (
-            <Card sx={{ maxWidth: 500 }}>
-              <CardActionArea>
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    {post.title}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {post.description}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          );
-        })}
+        {posts
+          .filter((post) => {
+            if (searchTerm == "") {
+              return post;
+            } else if (
+              post.title.toLowerCase().includes(searchTerm.toLowerCase())
+            ) {
+              return post;
+            }
+          })
+          .map((post) => {
+            return (
+              <Card sx={{ maxWidth: 500 }}>
+                <CardActionArea>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {post.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {post.description}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            );
+          })}
       </Box>
     </>
   );
