8fcb6e984afafe283ee8e8d582812ea5d0421c10
Dimitar <wopz1337@gmail.com>
2022-10-23 01:59:02 +0300

    Added Search Filter By User Full-Name

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, TextField, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import * as React from "react";
 import Table from "@mui/material/Table";
@@ -12,6 +12,8 @@ import AddUser from "../components/AddUser";
 
 const Users = () => {
   const [users, setUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
   useEffect(() => {
     fetch(
       "https://my-json-server.typicode.com/dimitarsabev00/Frond-End-FE-Tech-Challenge/users"
@@ -23,6 +25,15 @@ const Users = () => {
 
   return (
     <>
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <TextField
+          sx={{ width: "40rem", marginTop: "1rem" }}
+          placeholder="Search.. "
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+        />
+      </Box>
       <TableContainer component={Paper} sx={{ marginTop: "3rem" }}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
@@ -33,16 +44,27 @@ const Users = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {users.map((user) => (
-              <TableRow
-                key={user.id}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell>{user.id}</TableCell>
-                <TableCell>{user.name}</TableCell>
-                <TableCell>{user.email}</TableCell>
-              </TableRow>
-            ))}
+            {users
+              .filter((user) => {
+                if (searchTerm == "") {
+                  return user;
+                } else if (
+                  user.name.toLowerCase().includes(searchTerm.toLowerCase())
+                ) {
+                  return user;
+                }
+              })
+
+              .map((user) => (
+                <TableRow
+                  key={user.id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell>{user.id}</TableCell>
+                  <TableCell>{user.name}</TableCell>
+                  <TableCell>{user.email}</TableCell>
+                </TableRow>
+              ))}
           </TableBody>
         </Table>
       </TableContainer>
