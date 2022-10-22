aacd7bd69d061e16b256f2c9ba3e97239ed1790c
Dimitar <wopz1337@gmail.com>
2022-10-22 12:52:58 +0300

    Display All User Data In Table

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,5 +1,13 @@
 import { Box, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 const Users = () => {
   const [users, setUsers] = useState([]);
@@ -8,17 +16,42 @@ const Users = () => {
       .then((response) => response.json())
       .then((json) => setUsers(json));
   }, []);
+  console.log(users);
   return (
     <>
-      <Box>
-        {users.map((user) => {
-          return (
-            <>
-              <Typography variant="h6"> Email: {user.email}</Typography>
-            </>
-          );
-        })}
-      </Box>
+      <TableContainer component={Paper} sx={{ marginTop: "5rem" }}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>Email</TableCell>
+              <TableCell>Username</TableCell>
+              <TableCell>Company</TableCell>
+              <TableCell>City</TableCell>
+              <TableCell>Phone</TableCell>
+              <TableCell>Website</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {users.map((user) => (
+              <TableRow
+                key={user.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell>{user.id}</TableCell>
+                <TableCell>{user.name}</TableCell>
+                <TableCell>{user.email}</TableCell>
+                <TableCell>{user.username}</TableCell>
+                <TableCell>{user.company.name}</TableCell>
+                <TableCell>{user.address.city}</TableCell>
+                <TableCell>{user.phone}</TableCell>
+                <TableCell>{user.website}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   );
 };
